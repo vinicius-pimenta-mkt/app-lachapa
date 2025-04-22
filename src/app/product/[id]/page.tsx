4f7df6e5ed23aec3b01fb5d15ffcb6e3cd075f69
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-22 19:42:40 -0300

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -17,7 +17,7 @@ interface AdditionalWithQuantity extends Additional {
   quantity: number;
 }
 
-export default function ProductDetail({ params }: ProductDetailProps) {
+export default async function ProductDetail({ params }: ProductDetailProps) {
   const router = useRouter();
   const [quantity, setQuantity] = useState(1);
   const [selectedAdditionals, setSelectedAdditionals] = useState<AdditionalWithQuantity[]>([]);
