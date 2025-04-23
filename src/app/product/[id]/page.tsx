bd3219be2f69e930b3e6ae5cb7393732b1ae9be9
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-23 10:53:58 -0300

    Força tipagem com any para resolver conflito com PageProps

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import ProductDetailClient from './ProductDetailClient';
 
-export default function ProductPage({ params }: { params: { id: string } }) {
+export default function ProductPage({ params }: any) {
   return (
     <Suspense fallback={<div>Carregando...</div>}>
       <ProductDetailClient params={params} />
