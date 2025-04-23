29064cbe75a78c1c8b8320ca6d1ace6f0e5b8a6b
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-23 09:44:30 -0300

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,15 +1,7 @@
 import { Suspense } from 'react';
 import ProductDetailClient from './ProductDetailClient';
 
-// Interface para página do Next.js
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
-
-// Componente servidor assíncrono
-export default async function ProductPage({ params }: ProductPageProps) {
+export default function ProductPage({ params }: { params: { id: string } }) {
   return (
     <Suspense fallback={<div>Carregando...</div>}>
       <ProductDetailClient params={params} />
