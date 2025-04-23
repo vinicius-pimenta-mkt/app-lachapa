23c65f4bbc38c1be54036af70a8c7fffbf36ebf6
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-23 14:45:37 -0300

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,16 +1,10 @@
 import { Suspense } from 'react';
 import ProductDetailClient from './ProductDetailClient';
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
-
-export default function ProductPage({ params }: Params) {
+export default function ProductPage(props: any) {
   return (
     <Suspense fallback={<div>Carregando...</div>}>
-      <ProductDetailClient params={params} />
+      <ProductDetailClient params={props.params} />
     </Suspense>
   );
 }
