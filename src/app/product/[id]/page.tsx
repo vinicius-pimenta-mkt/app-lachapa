f4022c28618c2792f5b18cbff612bef80c6b6754
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-23 18:59:59 -0300

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,10 +1,13 @@
 import { Suspense } from 'react';
 import ProductDetailClient from './ProductDetailClient';
 
+// ⚠️ Removemos a tipagem explícita para evitar conflitos com PageProps/Promise<any>
 export default function ProductPage(props: any) {
+  const { params } = props;
+
   return (
     <Suspense fallback={<div>Carregando...</div>}>
-      <ProductDetailClient params={props.params} />
+      <ProductDetailClient params={params} />
     </Suspense>
   );
 }
