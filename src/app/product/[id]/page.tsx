7b69ae9b96422d3db0b5f0b94419bc22fc9a5393
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-26 10:08:07 -0300

    Add files via upload

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from 'react';
+import ProductDetailClient from './ProductDetailClient';
+
+export default function ProductPage({ params }: { params: { id: string } }) {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <ProductDetailClient params={params} />
+    </Suspense>
+  );
+}
