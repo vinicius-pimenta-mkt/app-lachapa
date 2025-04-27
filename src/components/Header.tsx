c2d37090d350fc972cf217407dd42f4081c59a1f
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-27 12:49:02 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,8 +48,8 @@ export default function Header() {
           <Link href="/" className="text-white hover:text-yellow-400 transition-colors">
             Cardápio
           </Link>
-          <Link href="/cart" className="relative" style={{ position: 'fixed', top: '20px', right: '20px', zIndex: '50' }}>
-            <button className="w-8 h-8 bg-white text-primary rounded-full flex items-center justify-center">
+          <Link href="/cart" className="relative" style={{ position: 'fixed', top: '12px', right: '12px', zIndex: '50' }}>
+            <button className="w-5 h-5 bg-white text-primary rounded-full flex items-center justify-center">
               <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
               </svg>
