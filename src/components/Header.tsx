ee96035b75586083b88a9b1dcea685ea932f6391
vinicius-pimenta-mkt <umanovavisao.portal@gmail.com>
2025-04-27 16:36:04 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
     <header className="la-chapa-header p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center py-10">
         <div className="header-logo-title">
-          <div className="la-chapa-logo ml-6 mt-4">
+          <div className="la-chapa-logo ml-7 mt-9">
             <Image 
               src="/images/logo.png" 
               alt="La Chapa Hamburgueria" 
@@ -49,8 +49,8 @@ export default function Header() {
             Cardápio
           </Link>
           <Link href="/cart" className="relative" style={{ position: 'fixed', top: '12px', right: '12px', zIndex: '50' }}>
-            <button className="w-5 h-5 bg-white text-primary rounded-full flex items-center justify-center">
-              <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <button className="w-3 h-3 bg-white text-primary rounded-full flex items-center justify-center">
+              <svg className="w-2 h-2 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
               </svg>
               Carrinho
